7637d9091aa41c403b94e274e27c848bc73428cd
sheikh.shabab <sheikh.shabab@bjitgroup.com>
2025-03-19 14:54:54 +0600

    Update
    - show list
    - with pagination
    - click on row to view details
    - click on predict button for prediction

--- .next/static/chunks/src_app_page_598cbcb5.js
+++ .next/static/chunks/src_app_page_598cbcb5.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_598cbcb5.js",
   "chunks": [
-    "static/chunks/node_modules_next_dist_ea69acce._.js",
-    "static/chunks/src_app_page_module_d12eb71c.css"
+    "static/chunks/_aed4e2c2._.js",
+    "static/chunks/src_app_page_909bcf69.css"
   ],
   "source": "dynamic"
 });
